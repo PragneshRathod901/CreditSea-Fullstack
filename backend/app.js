a51ef44849ed9147386f55af3a87062c69a02538
Pragnesh Rathod <39561106+PragneshRathod901@users.noreply.github.com>
2025-02-07 20:09:22 +0530

    initialization

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const DB_URI = "mongodb://127.0.0.1:27017";
+
+const app = express();
+const PORT = 8082;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Connected to DB at", DB_URI))
+  .catch((error) => console.log("Failed to connect to DB\n", error));
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server Listening at", PORT);
+});
