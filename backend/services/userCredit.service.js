9e8bc70230c5cdadde8ce45012f90a3734a76766
Pragnesh Rathod <39561106+PragneshRathod901@users.noreply.github.com>
2025-02-08 01:04:47 +0530

    added xl parser

--- backend/services/userCredit.service.js
+++ backend/services/userCredit.service.js
@@ -1,1 +1,11 @@
 const userCreditModel = require("../models/userCredit.model");
+const xml2js = require("xml2js");
+
+class UserCreditsService {
+  uploadXmlData = async (data) => {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  };
+}
