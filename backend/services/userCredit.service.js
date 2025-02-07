20bf9820978e58e47e6da9d0e3a3c1a95d0deefa
Pragnesh Rathod <39561106+PragneshRathod901@users.noreply.github.com>
2025-02-08 01:15:44 +0530

    reverting mistake

--- backend/services/userCredit.service.js
+++ backend/services/userCredit.service.js
@@ -1,11 +1,1 @@
 const userCreditModel = require("../models/userCredit.model");
-const xml2js = require("xml2js");
-
-class UserCreditsService {
-  uploadXmlData = async (data) => {
-    try {
-    } catch (error) {
-      throw error;
-    }
-  };
-}
